efaeac28ab09bf8a5682b79da4ed38894dc90b7f
Breno <gd-gameplay@hotmail.com>
2020-02-05 01:28:29 -0300

    CRUD Scaffolding

---
+++ WebApplication1/Data/SaleWebApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SaleWebApplication.Models;
+
+namespace SaleWebApplication.Data
+{
+    public class SaleWebApplicationContext : DbContext
+    {
+        public SaleWebApplicationContext (DbContextOptions<SaleWebApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SaleWebApplication.Models.Department> Department { get; set; }
+    }
+}
